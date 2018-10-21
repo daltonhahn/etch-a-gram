d84188c0bfca7689ed51a57e6d8f13a3dc998298
meagankb <meagankb@ksu.edu>
2018-10-20 21:34:57 -0500

    just got an A in algorithms

--- photo_stuff/path.py
+++ photo_stuff/path.py
@@ -11,36 +11,30 @@ with open("./binary_output/square_output.txt", "r") as file: # open file
             readLine.append(c) # append each character of the line
         screen.append(readLine) # append each line, as a list, to screen
 
-
-
-
 plane = np.asarray(screen, dtype=int)
 coordinates = np.argwhere(plane==1)
-print(coordinates)
 
+path = []
 
+# special case: calculating the shortest distance from (0,0) to closest 1
+# origin = np.array([0,0])
+current_coord = [0, 0] # start at the origin
+next_coord = [0, 0]
 
+# i represents the number of times calculating the distance between each coordinates
+# or making a step in the path
+for i in range(len(coordinates)):
+    min_dist = 4500 # close enough to max distance you can get to
 
+    # going through coordinates we haven't gone to
+    for j in range(len(coordinates)):
+        coord = coordinates[j];
+        dist = np.linalg.norm(coord - current_coord)
+        if min_dist > dist:
+            min_dist = dist
+            next_coord = coord
+            min_list_count = j
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Writes the results of the 2-D array to a file.
-# Saved for testing
-# f = open("text_to_array.txt", "w")
-# for row in screen:
-#     for col in row:
-#         f.write(col)
-#     f.write('\n')
-#
-# f.close()
+    path.append(next_coord) # add the next step in our path
+    current_coord = next_coord # use to calculate where to go next
+    coordinates = np.delete(coordinates, min_list_count, axis=0) # remove the step we took
