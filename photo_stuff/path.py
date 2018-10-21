39c1ab8ea6eaf1cb712f7a686ff18cea7a1e6202
daltonhahn <hahnd11@gmail.com>
2018-10-20 20:18:34 -0500

    Adding beginning of path drawing

--- photo_stuff/path.py
+++ photo_stuff/path.py
@@ -1,12 +1,39 @@
+import numpy as np
+
+
 # Takes a file and turns it into a 2-D array
 screen = [] # the 2-D array containing the file
 
-with open("output.txt", "r") as file: # open file
+with open("./binary_output/square_output.txt", "r") as file: # open file
     for line in file: # loop through file lines
         readLine = []
         for c in line.strip(): # removes newline characters
             readLine.append(c) # append each character of the line
         screen.append(readLine) # append each line, as a list, to screen
+
+
+
+
+plane = np.asarray(screen, dtype=int)
+coordinates = np.argwhere(plane==1)
+print(coordinates)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Writes the results of the 2-D array to a file.
 # Saved for testing
