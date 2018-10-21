2fac495842088f00f2fbeb0b870ec29804033fb8
meagankb <meagankb@ksu.edu>
2018-10-20 22:00:17 -0500

    path is litty lit

--- photo_stuff/path.py
+++ photo_stuff/path.py
@@ -13,13 +13,17 @@ with open("./binary_output/square_output.txt", "r") as file: # open file
 
 plane = np.asarray(screen, dtype=int)
 coordinates = np.argwhere(plane==1)
+print(coordinates)
+print('\n\n\n\n\n\n\n\n\n')
 
 path = []
 
 # special case: calculating the shortest distance from (0,0) to closest 1
 # origin = np.array([0,0])
 current_coord = [0, 0] # start at the origin
-next_coord = [0, 0]
+path.append(current_coord) # make the origin the start of the path
+
+next_coord = [0, 0] # initialize next_coord
 
 # i represents the number of times calculating the distance between each coordinates
 # or making a step in the path
@@ -38,3 +42,13 @@ for i in range(len(coordinates)):
     path.append(next_coord) # add the next step in our path
     current_coord = next_coord # use to calculate where to go next
     coordinates = np.delete(coordinates, min_list_count, axis=0) # remove the step we took
+
+f = open("path.txt", "w")
+for i in range(len(path) - 1):
+    step1 = path[i]
+    step2 = path[i+1]
+    x_dist = step2[1] - step1[1]
+    y_dist = step2[0] - step1[0]
+    f.write("{0} {1}\n".format(x_dist, y_dist))
+
+f.close()
